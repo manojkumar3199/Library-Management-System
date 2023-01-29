da6442f784ba551cce278a98c2612b69d1b3db8d
Manoj Kumar <manojkumarus03@gmail.com>
2023-01-28 22:14:42 -0800

    configure cors

--- src/main/java/com/manoj/config/WebMvcConfig.java
+++ src/main/java/com/manoj/config/WebMvcConfig.java
@@ -2,15 +2,16 @@ package com.manoj.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class StaticConfig implements WebMvcConfigurer {
+public class WebMvcConfig implements WebMvcConfigurer {
 
 	private String uploadImagesDir;
 
-	public StaticConfig(@Value("${project.upload.images}") String uploadImagesDir) {
+	public WebMvcConfig(@Value("${project.upload.images}") String uploadImagesDir) {
 		super();
 		this.uploadImagesDir = uploadImagesDir;
 	}
@@ -18,6 +19,11 @@ public class StaticConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/uploads/**").addResourceLocations("file:" + uploadImagesDir + "/");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
 	}
 
 }
